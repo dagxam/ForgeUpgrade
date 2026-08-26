4fd36949931e99289915c29ea5e145e0af498a0a
dagxam <dagxam@gmail.com>
2026-08-26 21:23:18 +0300

    Расширен главный класс и подключён фундамент улучшений

--- src/main/java/ru/dagxam/forgeupgrade/ForgeUpgrade.java
+++ src/main/java/ru/dagxam/forgeupgrade/ForgeUpgrade.java
@@ -1,29 +1,41 @@
 package ru.dagxam.forgeupgrade;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.dagxam.forgeupgrade.command.ForgeUpgradeCommand;
+import ru.dagxam.forgeupgrade.recipe.RecipeManager;
+import ru.dagxam.forgeupgrade.upgrade.UpgradeManager;
 
+/** Главный класс ForgeUpgrade. */
 public final class ForgeUpgrade extends JavaPlugin {
 
-    private static ForgeUpgrade instance;
+    private UpgradeManager upgradeManager;
 
     @Override
     public void onEnable() {
-        instance = this;
-
         saveDefaultConfig();
-        getLogger().info("========================================");
-        getLogger().info("ForgeUpgrade включён.");
-        getLogger().info("Система улучшения предметов загружена.");
-        getLogger().info("Все сообщения плагина используются на русском языке.");
-        getLogger().info("========================================");
+        saveResource("upgrades.yml", false);
+        saveResource("messages.yml", false);
+
+        upgradeManager = new UpgradeManager(this);
+        new RecipeManager(this, upgradeManager).registerRecipes();
+
+        ForgeUpgradeCommand command = new ForgeUpgradeCommand(this, upgradeManager);
+        if (getCommand("forgeupgrade") != null) {
+            getCommand("forgeupgrade").setExecutor(command);
+            getCommand("forgeupgrade").setTabCompleter(command);
+        }
+
+        getLogger().info("ForgeUpgrade успешно включён.");
+        getLogger().info("Загружено улучшений: " + upgradeManager.getTypes().size());
+        getLogger().info("Крафты улучшений успешно зарегистрированы.");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("ForgeUpgrade выключен.");
+        getLogger().info("ForgeUpgrade успешно выключен.");
     }
 
-    public static ForgeUpgrade getInstance() {
-        return instance;
+    public UpgradeManager getUpgradeManager() {
+        return upgradeManager;
     }
 }
