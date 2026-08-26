389d3a68733935d5672811244d71dc5b8abd73d8
dagxam <dagxam@gmail.com>
2026-08-26 21:26:41 +0300

    Подключена система применения улучшений

--- src/main/java/ru/dagxam/forgeupgrade/ForgeUpgrade.java
+++ src/main/java/ru/dagxam/forgeupgrade/ForgeUpgrade.java
@@ -2,13 +2,16 @@ package ru.dagxam.forgeupgrade;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.dagxam.forgeupgrade.command.ForgeUpgradeCommand;
+import ru.dagxam.forgeupgrade.listener.UpgradeListener;
 import ru.dagxam.forgeupgrade.recipe.RecipeManager;
+import ru.dagxam.forgeupgrade.upgrade.UpgradeApplier;
 import ru.dagxam.forgeupgrade.upgrade.UpgradeManager;
 
 /** Главный класс ForgeUpgrade. */
 public final class ForgeUpgrade extends JavaPlugin {
 
     private UpgradeManager upgradeManager;
+    private UpgradeApplier upgradeApplier;
 
     @Override
     public void onEnable() {
@@ -17,7 +20,12 @@ public final class ForgeUpgrade extends JavaPlugin {
         saveResource("messages.yml", false);
 
         upgradeManager = new UpgradeManager(this);
+        upgradeApplier = new UpgradeApplier(this);
+
         new RecipeManager(this, upgradeManager).registerRecipes();
+        getServer().getPluginManager().registerEvents(
+                new UpgradeListener(upgradeManager, upgradeApplier), this
+        );
 
         ForgeUpgradeCommand command = new ForgeUpgradeCommand(this, upgradeManager);
         if (getCommand("forgeupgrade") != null) {
@@ -28,6 +36,7 @@ public final class ForgeUpgrade extends JavaPlugin {
         getLogger().info("ForgeUpgrade успешно включён.");
         getLogger().info("Загружено улучшений: " + upgradeManager.getTypes().length);
         getLogger().info("Крафты улучшений успешно зарегистрированы.");
+        getLogger().info("Система применения улучшений успешно включена.");
     }
 
     @Override
@@ -37,5 +46,9 @@ public final class ForgeUpgrade extends JavaPlugin {
 
     public UpgradeManager getUpgradeManager() {
         return upgradeManager;
+    }
+
+    public UpgradeApplier getUpgradeApplier() {
+        return upgradeApplier;
     }
 }
